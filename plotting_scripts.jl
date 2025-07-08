920fd7d9005c3727c748db1600d892980984d0af
Pavel Zhelnin <pzhelnin@g.harvard.edu>
2025-07-08 13:47:19 -0400

    new save

--- plotting_scripts.jl
+++ plotting_scripts.jl
@@ -5,7 +5,9 @@ using LinearAlgebra
 using Statistics
 using FFTW 
 using QuadGK
+using StatsBase
 using SpecialFunctions
+
 
 function adiabatic_or_nah(sim::SimulationConfig)
     P = exp(-(π/2)*(sim.g^2)*(1/sim.α))
@@ -17,31 +19,70 @@ function adiabatic_or_nah(sim::SimulationConfig)
         return "intermediate"
     end
 end
-function spin_animation(times,SX,SY,s::SimulationConfig)
-    q_anim = @animate for (idx,t) in enumerate(times) 
-        B = normalize([s.g,0,s.α*t])
-        #B = [0,s.g * -sin(t),s.α * cos(t)]
-        sxs = [sx[idx] for sx in SX]
-        sys = [sy[idx] for sy in SY]
 
-        scatter([sxs],[sys], 
-        label="time: $(t)",aspect_ratio=:equal, 
-        xlims=(-1,1), ylims=(-1,1),
+function plot_xy(sol,s::SimulationConfig;alpha=alpha)
+    sx,sy,sz = [],[],[]
+
+    for (idx,t) in enumerate(sol.t)
+        ψ = [sol[1,idx],sol[2,idx]]
+        push!(sx, real(ψ'*σ[1]*ψ))
+        push!(sy, real(ψ'*σ[2]*ψ))
+        push!(sz, real(ψ'*σ[3]*ψ))
+    end
+    println(maximum(sy))
+    println(maximum(sx.^2 .+ sy.^2 .+ sz.^2))
+    title = adiabatic_or_nah(s)
+    time = s.t_i:s.Δt:s.t_f
+    scatter(
+        sx[1:end],sy[1:end],marker_z=sz[1:end],
+        # sx[1:length(time)-1:end], 
+        # sy[1:length(time)-1:end], 
+        # marker_z=rad2deg.(cos.(sz[1:length(time)-1:end])),
+        title="$(title) transition",
         legend=:topleft,
         framestyle=:box,
+        label="",
+        xlimits=(-1,1),
+        ylimits=(-1,1),
+        aspect_ratio=:equal,
         # left_margin=5mm,
         # top_margin=5mm,
         # right_margin=5mm,
         # bottom_margin=5mm,
         fontfamily="Computer Modern",
+        msw=0,
+        # ms=10,
+        xlabel="x",
+        ylabel="y",
         tickfontsize=12,
         size=(600, 400)
-        )
-        scatter!([B[1]], [B[2]], label="", xlims=(-1,1), ylims=(-1,1))
-        #scatter!([positions[:,1]], [sy[idx]], label="", xlims=(-1,1), ylims=(-1,1))
-    end
-    return q_anim 
-end 
+    )
+end
+# function spin_animation(times,SX,SY,s::SimulationConfig)
+#     q_anim = @animate for (idx,t) in enumerate(times) 
+#         B = normalize([s.g,0,s.α*t])
+#         #B = [0,s.g * -sin(t),s.α * cos(t)]
+#         sxs = [sx[idx] for sx in SX]
+#         sys = [sy[idx] for sy in SY]
+
+#         scatter([sxs],[sys], 
+#         label="time: $(t)",aspect_ratio=:equal, 
+#         xlims=(-1,1), ylims=(-1,1),
+#         legend=:topleft,
+#         framestyle=:box,
+#         # left_margin=5mm,
+#         # top_margin=5mm,
+#         # right_margin=5mm,
+#         # bottom_margin=5mm,
+#         fontfamily="Computer Modern",
+#         tickfontsize=12,
+#         size=(600, 400)
+#         )
+#         scatter!([B[1]], [B[2]], label="", xlims=(-1,1), ylims=(-1,1))
+#         #scatter!([positions[:,1]], [sy[idx]], label="", xlims=(-1,1), ylims=(-1,1))
+#     end
+#     return q_anim 
+# end 
 
 function entropy_plot(SX,SY,SZ,time)
     entropy = [] 
@@ -80,28 +121,31 @@ function amplitudes_plot(sol,s::SimulationConfig)
         w = quadgk(x -> sqrt(x^2+s.g^2),0,τ)[1]
         σ[idx] = w/sqrt(2*sim.α*abs(wc))
     end 
-
-    plot(σ, 
+    f = Figure() 
+    ax = Axis(f[1, 1],aspect=1)
+    xlims!(ax,(minimum(σ),maximum(σ)))
+    lines!(ax,σ, 
         real.(sqrt.(c2)), 
-        title = "$(title) transition", 
-        label="|c1|^2",
-        legend=:right,
-        framestyle=:box,
-        xlimits=(-15,15),
+        # title = "$(title) transition", 
+        # label="|c1|^2",
+        # legend=:right,
+        # framestyle=:box,
+        # xlimits=(-15,15),
         #ylimits=(0,1e-1),
         # left_margin=5mm,
         # top_margin=5mm,
         # right_margin=5mm,
         # bottom_margin=5mm,
-        fontfamily="Computer Modern",
-        xlabel="time",
-        ylabel="amplitude",
-        tickfontsize=12,
-        size=(600, 400)
+        # fontfamily="Computer Modern",
+        # xlabel="time",
+        # ylabel="amplitude",
+        # tickfontsize=12,
+        # size=(600, 400)
         )
     
     #plot!(sol.t, real.(sqrt.(c2)), label="|c2|^2")
-    hline!([exp(-pi/(2*sim.α))], label="final c1 asymptote")
+    hlines!(ax,[exp(-pi/(2*sim.α))], label="final c1 asymptote")
+    display(f)
 end 
 
 function super_amplitudes_plot(sol,s::SimulationConfig)
@@ -169,24 +213,24 @@ function energies_plot(sol,s::SimulationConfig)
     plot!(sol.t, e2/maximum(abs.(e2)), label="E2")
 end
 
-function single_spin_animation(sol,s::SimulationConfig)
-    sx,sy,sz = [],[],[]
+# function single_spin_animation(sol,s::SimulationConfig)
+#     sx,sy,sz = [],[],[]
 
-    for (idx,t) in enumerate(sol.t)
-        ψ = [sol[1,idx],sol[2,idx]]
-        push!(sx, real(ψ'*σ[1]*ψ))
-        push!(sy, real(ψ'*σ[2]*ψ))
-        push!(sz, real(ψ'*σ[3]*ψ))
-    end
+#     for (idx,t) in enumerate(sol.t)
+#         ψ = [sol[1,idx],sol[2,idx]]
+#         push!(sx, real(ψ'*σ[1]*ψ))
+#         push!(sy, real(ψ'*σ[2]*ψ))
+#         push!(sz, real(ψ'*σ[3]*ψ))
+#     end
 
-    q_anim = @animate for (idx,t) in enumerate(sol.t) 
-        #B = [0,s.g * -sin(t),s.α * cos(t)]
-        B = normalize([s.g,0,s.α*t])
-        scatter([B[1]],[B[2]], label="time: $(t)", xlims=(-1,1), ylims=(-1,1))
-        scatter!([sx[idx]], [sy[idx]], label="", xlims=(-1,1), ylims=(-1,1))
-    end 
-    return q_anim 
-end 
+#     q_anim = @animate for (idx,t) in enumerate(sol.t) 
+#         #B = [0,s.g * -sin(t),s.α * cos(t)]
+#         B = normalize([s.g,0,s.α*t])
+#         scatter([B[1]],[B[2]], label="time: $(t)", xlims=(-1,1), ylims=(-1,1))
+#         scatter!([sx[idx]], [sy[idx]], label="", xlims=(-1,1), ylims=(-1,1))
+#     end 
+#     return q_anim 
+# end 
 
 function single_entropy_plot(sol)
     sx,sy,sz = [],[],[]
@@ -211,18 +255,24 @@ function single_entropy_plot(sol)
 
         push!(entropy,-tr(ρ*log(ρ)))
     end 
-    scatter(sol.t, real.(entropy), ylims=(0,log(2)+0.1),label="single qubit S",title="entropy",
-        legend=:topleft,
-        framestyle=:box,
+    f = Figure() 
+    ax = Axis(f[1, 1], aspect=1)
+    ylims!(ax,(0,log(2)+0.1))
+    scatter!(ax, sol.t, real.(entropy), 
+        # label="single qubit S",
+        # title="entropy",
+        # legend=:topleft,
+        # framestyle=:box,
         # left_margin=5mm,
         # top_margin=5mm,
         # right_margin=5mm,
         # bottom_margin=5mm,
-        fontfamily="Computer Modern",
-        tickfontsize=12,
-        size=(600, 400)
+        # fontfamily="Computer Modern",
+        # tickfontsize=12,
+        # size=(600, 400)
         )
-    hline!([log(2)], label="max S") 
+    hlines!(ax,[log(2)]) 
+    display(f)
 end 
 
 function cg_entropy_plot(sol,s::SimulationConfig)
@@ -597,7 +647,7 @@ function projected_entropy(sol,s::SimulationConfig)
         push!(sz, real(ψ'*σ[3]*ψ))
     end
 
-    display("this is sz: $(sz)")
+    #display("this is sz: $(sz)")
     entropy = [] 
     fidelity = []
     targets = []
@@ -641,55 +691,61 @@ function projected_entropy(sol,s::SimulationConfig)
     c_n = exp(-(π/2)*(sim.g^2) /sim.α) 
     # final_vector = [0,0,cos(theta)-0.002]
     final_vector = [0,0,1-2*c_n^2]
-    display(final_vector)
+    #display(final_vector)
     α = 1 - 2*c_n^2
     X,Z = s.g,s.α*sol.t[end]
     final_vector = zeros(3)
     final_vector[3] = (Z*α * sqrt((Z^2 * α^2) -(X^2 +Z^2)*(α^2 -X^2)))/(X^2 +Z^2)
     #final_vector[3] = 1
-    display(final_vector)
+    # display(final_vector)
     final_ρ = 1/2*(I + (final_vector[1]*σ[1] + final_vector[2]*σ[2] + final_vector[3]*σ[3]))
     final_S = -tr(final_ρ*log(final_ρ))
     if c_n == 0 
         final_S = 0 
     end
-    display(final_S)
-    display(entropy[end])
-    display(real.(entropy[1]))
-    display(entropy[end-10])
-    display(targets[end])
+    # display(final_S)
+    # display(entropy[end])
+    # display(real.(entropy[1]))
+    # display(entropy[end-10])
+    # display(targets[end])
     title = adiabatic_or_nah(s)
-    plt = plot(sol.t, 
+    f = Figure() 
+    ax = Axis(f[1,1])
+    lines!(ax, 
          real.(entropy), 
-        ylims=(-0.05,log(2)+0.1),
-        label="single qubit S",
-        title="$(title) transition",
-        legend=:topleft,
-        framestyle=:box,
-        # left_margin=5mm,
-        # top_margin=5mm,
-        # right_margin=5mm,
-        # bottom_margin=5mm,
-        fontfamily="Computer Modern",
-        msw=0,
-        ms=2,
-        xlabel="time",
-        ylabel="entropy",
-        tickfontsize=12,
-        size=(600, 400)
+        # ylims=(-0.05,log(2)+0.1),
+        # label="single qubit S",
+        # title="$(title) transition",
+        # legend=:topleft,
+        # framestyle=:box,
+        # # left_margin=5mm,
+        # # top_margin=5mm,
+        # # right_margin=5mm,
+        # # bottom_margin=5mm,
+        # fontfamily="Computer Modern",
+        # msw=0,
+        # ms=2,
+        # xlabel="time",
+        # ylabel="entropy",
+        # tickfontsize=12,
+        # size=(600, 400)
         )
-        hline!(plt,[log(2)], label="max S") 
-        hline!(plt,[real.(final_S)], label="final S")
-    plt2 =  plot(sol.t, 
+        hlines!(ax,[log(2)], label="max S") 
+        hlines!(ax,[real.(final_S)], label="final S")
+        println(final_S)
+        ax2 = Axis(f[1,2])
+    lines!(ax2,sol.t, 
         (real.(final_ρs)).^2,
         #ylims=(-0.05,log(2)+0.1),
-        ylabel="fidelity",
-        xlabel="time",
-        ylimits=(0,1),
-        tickfontsize=12,)
+        # ylabel="fidelity",
+        # xlabel="time",
+        # ylimits=(0,1),
+        # tickfontsize=12,)
+    )
+    display(f)
     #scatter!(plt2, sol.t, (real.(final_ρs)).^2 , label="final ρ")
-     
-    display(plot(plt, plt2, layout = (1, 2), legend = false))
+    #display(f)
+    #display(plot(plt, plt2, layout = (1, 2), legend = false))
 
     # for cg in cgs 
     #     cg_entropy = []
@@ -879,50 +935,50 @@ function plot_frequencies(sol,s::SimulationConfig)
     plot(time_domain, freq_domain, layout = (2,1))
 end
 
-function polar_plot(sol, s::SimulationConfig)
+# function polar_plot(sol, s::SimulationConfig)
 
-    SX,SY,SZ = [],[],[]
+#     SX,SY,SZ = [],[],[]
 
-    for (idx,t) in enumerate(sol.t)
-        ψ = [sol[1,idx],sol[2,idx]]
-        push!(SX, real(ψ'*σ[1]*ψ))
-        push!(SY, real(ψ'*σ[2]*ψ))
-        push!(SZ, real(ψ'*σ[3]*ψ))
-    end
+#     for (idx,t) in enumerate(sol.t)
+#         ψ = [sol[1,idx],sol[2,idx]]
+#         push!(SX, real(ψ'*σ[1]*ψ))
+#         push!(SY, real(ψ'*σ[2]*ψ))
+#         push!(SZ, real(ψ'*σ[3]*ψ))
+#     end
 
-    q_anim = @animate for (idx,t) in enumerate(sol.t) 
-        B = normalize([s.g,0,s.α*t])
-        #B = [0,s.g * -sin(t),s.α * cos(t)]
-        sxs = [SX[idx]]
-        sys = [SY[idx]]
-        szs = [SZ[idx]]
+#     q_anim = @animate for (idx,t) in enumerate(sol.t) 
+#         B = normalize([s.g,0,s.α*t])
+#         #B = [0,s.g * -sin(t),s.α * cos(t)]
+#         sxs = [SX[idx]]
+#         sys = [SY[idx]]
+#         szs = [SZ[idx]]
 
-        r = sqrt.(sxs.^2 + sys.^2 + szs.^2)
-        θ = acos.(szs ./ r) 
+#         r = sqrt.(sxs.^2 + sys.^2 + szs.^2)
+#         θ = acos.(szs ./ r) 
 
      
-        B_r = sqrt.(B[1]^2 + B[2]^2 + B[3]^2)
-        B_θ = acos(B[3]/B_r)
+#         B_r = sqrt.(B[1]^2 + B[2]^2 + B[3]^2)
+#         B_θ = acos(B[3]/B_r)
 
-        scatter([θ],[r],proj=:polar, 
-        label="time: $(t)",#aspect_ratio=:equal, 
-        xlims=(0,1), ylims=(0,1),
-        legend=:topleft,
-        framestyle=:box,
-        # left_margin=5mm,
-        # top_margin=5mm,
-        # right_margin=5mm,
-        # bottom_margin=5mm,
-        fontfamily="Computer Modern",
-        tickfontsize=12,
-        size=(600, 400)
-        )
-        scatter!([B_θ], [B_r], proj=:polar,label="B")
-        #scatter!([B[1]], [B[2]], label="", xlims=(-1,1), ylims=(-1,1))
-        #scatter!([positions[:,1]], [sy[idx]], label="", xlims=(-1,1), ylims=(-1,1))
-    end
-    return q_anim 
-end
+#         scatter([θ],[r],proj=:polar, 
+#         label="time: $(t)",#aspect_ratio=:equal, 
+#         xlims=(0,1), ylims=(0,1),
+#         legend=:topleft,
+#         framestyle=:box,
+#         # left_margin=5mm,
+#         # top_margin=5mm,
+#         # right_margin=5mm,
+#         # bottom_margin=5mm,
+#         fontfamily="Computer Modern",
+#         tickfontsize=12,
+#         size=(600, 400)
+#         )
+#         scatter!([B_θ], [B_r], proj=:polar,label="B")
+#         #scatter!([B[1]], [B[2]], label="", xlims=(-1,1), ylims=(-1,1))
+#         #scatter!([positions[:,1]], [sy[idx]], label="", xlims=(-1,1), ylims=(-1,1))
+#     end
+#     return q_anim 
+# end
 
 function a_θ(n,w_c,w)
     return (1/2*im)*((factorial(n) * im^(n+1))/2π) * (1/(w - w_c)^(n+1) - 1/(w - adjoint(w_c))^(n+1))
@@ -1211,4 +1267,156 @@ function plot_superA_zenith(sol,s)
     plot(plt, plt2, plt3, plt4, layout = (2, 2), legend = false)
 end 
 
-    
+function decompose(v)
+    x = 1/2 * tr([0 1; 1 0]*v)
+    y = 1/2 * tr([0 -im; im 0]*v)
+    z = 1/2 * tr([1 0; 0 -1]*v)
+
+return real.([x,y,z])
+end 
+
+function entropy_calc(SX,SY,SZ,H)
+    hdotp = dot(normalize(decompose(H)),[SX,SY,SZ])
+    ρ = hdotp * normalize(decompose(H))
+    S = 1/2*(I + (ρ[1]*σ[1] + ρ[2]*σ[2] + ρ[3]*σ[3]))
+
+    if real(-tr(S*log(S))) > 0
+        return real(-tr(S*log(S)))
+    else 
+        return 0 
+    end
+end
+
+function entropy_map(f,ax,SX,SY,SZ, s::SimulationConfig)
+
+    entropies = zeros(length(SX))
+    for idx in eachindex(SX)
+        first_sx,first_sy,first_sz,last_sx,last_sy,last_sz = SX[idx][1],SY[idx][1],SZ[idx][1],SX[idx][end],SY[idx][end],SZ[idx][end]
+        first_H = [s.α*s.t_i 1; 1  -s.α*s.t_i]
+        last_H = [s.α*s.t_f 1; 1  -s.α*s.t_f]
+        entropies[idx] = entropy_calc(last_sx,last_sy,last_sz,last_H) - entropy_calc(first_sx,first_sy,first_sz,first_H) 
+    end 
+    xs = getindex.(SX,1)
+    ys = getindex.(SY,1)
+
+    h = fit(Histogram, (xs, ys), weights(entropies), nbins=(20,20))
+    n = fit(Histogram, (xs, ys), nbins=(20,20)).weights
+    #h = normalize(h, mode=:probability)
+    # Modify the weights of the histogram
+    edges_x = h.edges[1]  # x edges
+    edges_y = h.edges[2]  # y edges
+
+    # ws = reshape(h.weights, length(edges_x)-1, length(edges_y)-1)
+    ws = h.weights
+
+    # Calculate the bin centers
+    x_centers = [(edges_x[i] + edges_x[i+1]) / 2 for i in 1:length(edges_x)-1]
+    y_centers = [(edges_y[i] + edges_y[i+1]) / 2 for i in 1:length(edges_y)-1]
+
+    # Iterate over bin centers to set specific conditions
+    for i in 1:length(x_centers)
+        for j in 1:length(y_centers)
+            # Check if bin center lies outside the circle x^2 + y^2 = 1
+            if x_centers[i]^2 + y_centers[j]^2 > 1
+                # Set weight to NaN if the bin count is zero
+                if ws[i, j] == 0
+                    ws[i, j] = NaN
+                end
+            else 
+                if ws[i,j] == 0
+                    try 
+                        ws[i, j] = ((ws[i+1,j]/n[i+1,j]) + (ws[i-1,j]/n[i-1,j]) + (ws[i,j+1]/n[i,j+1]) + (ws[i,j-1]/n[i,j-1])
+                        + (ws[i+1,j+1]/n[i+1,j+1]) + (ws[i+1,j-1]/n[i+1,j-1]) + (ws[i-1,j+1]/n[i-1,j+1]) + (ws[i-1,j-1]/n[i-1,j-1])) / 8
+                        continue 
+                    catch 
+                        ws[i, j] = NaN
+                    end 
+                end 
+            end 
+            ws[i,j] = ws[i,j]/n[i,j]
+        end
+    end
+
+    weighs = vcat(ws...)
+    weighs = weighs[.!isnan.(weighs)]
+    weighs = weighs[.!isinf.(weighs)]
+    cb_bins= minimum(weighs):0.001:maximum(weighs)
+    colorbar_ticks=(cb_bins, string.(round.(cb_bins; digits=4)))
+    colorbar_ticks = string.(-1:0.1:1)
+    #ax = Axis(f[1, 1],aspect=1)
+    xlims!(ax,(-1., 1.))
+    ylims!(ax,(-1., 1.))
+    println(edges_x)
+    heatmap!(ax,edges_x,edges_y, transpose(h.weights),colorrange=(-0.6,0.6),colormap=cgrad(:vik))
+            # aspect_ratio=:equal,
+            # xlimits=(-1.,1.),
+            # ylimits=(-1.,1.),
+            # title="ϵ = $(sim.α)",
+            # xlabel="x",
+            # ylabel="y",
+            # colorbar_ticks=colorbar_ticks,
+            # colorbar_title="Entropy",
+            # fontfamily="Computer Modern",
+            # color=cgrad(:vik))
+end 
+
+function sphere_plot(ax,entropy_ax,SX,SY,SZ,s::SimulationConfig)
+    n = 45
+    u = range(0,stop=2*π,length=n);
+    v = range(0,stop=π,length=n);
+    sx = zeros(n,n); sy = zeros(n,n); sz = zeros(n,n)
+    for i in 1:n
+        for j in 1:n
+            sx[i,j] = cos.(u[i]) * sin(v[j]);
+            sy[i,j] = sin.(u[i]) * sin(v[j]);
+            sz[i,j] = cos(v[j]);
+        end
+    end
+
+    H_exp_x = zeros(length(s.t_i:s.Δt:s.t_f))
+    H_exp_y = zeros(length(s.t_i:s.Δt:s.t_f))
+    H_exp_z = zeros(length(s.t_i:s.Δt:s.t_f))
+    entropy = zeros(length(s.t_i:s.Δt:s.t_f))
+    for (idx,t) in enumerate(s.t_i:s.Δt:s.t_f)
+        H_exp_x[idx],H_exp_y[idx],H_exp_z[idx] = normalize(decompose([s.α*t 1; 1 -s.α*t]))
+        entropy[idx] = entropy_calc(SX[1][idx],SY[1][idx],SZ[1][idx],[s.α*t 1; 1 -s.α*t])
+    end
+
+    p2 = surface!(ax,
+    sx,sy,sz,colormap=cgrad([:gray], 7), transparency = true, alpha = 0.25)
+    scale = 1.5
+    scale!(p2, scale,scale,scale) 
+    #   colormap=cgrad([:purple], 7),
+    #   shading=NoShading,
+    # ) 
+    #ax.show_axis = false
+    #wireframe!(ax, sx, sy, sz)
+    # p2 = scatter!(ax, exp_x,exp_y,exp_z, color=:red,markersize=3.5)
+    # scale!(p2, s,s,s) 
+    space = 1
+    # if s.t_i == -50
+    #     space = 100
+    #     p2 = lines!(ax, SX[1][1:1:end],SY[1][1:1:1],SZ[1][1:1:end], color=:red,linewidth=2)
+    #     scale!(p2, scale,scale,scale) 
+    #     p2 = scatter!(ax, SX[1][5010:space:5010+400],SY[1][5010:space:5010+400],SZ[1][5010:space:5010+400], color=:red)
+    #     scale!(p2, scale,scale,scale) 
+    #     p2 = lines!(ax, H_exp_x[2:end],H_exp_y[2:end],H_exp_z[2:end], color=:blue,linewidth=2)
+    #     scale!(p2, scale,scale,scale) 
+
+    #     xlims!(-10,10)
+    #     lines!(entropy_ax,s.t_i:s.Δt:s.t_f,entropy,color=:red)
+    #     return 
+    # end 
+    p2 = lines!(ax, SX[1][1:space:end],SY[1][1:space:end],SZ[1][1:space:end], color=:red,linewidth=2)
+    scale!(p2, scale,scale,scale) 
+    # p2 = lines!(ax, SX[1][1:1:5010],SY[1][1:1:5010],SZ[1][1:1:5010], color=:red,linewidth=2)
+    # scale!(p2, scale,scale,scale) 
+    # p2 = lines!(ax, SX[1][5010:space:5010+400],SY[1][5010:space:5010+400],SZ[1][5010:space:5010+400], color=:red,linewidth=2)
+    # scale!(p2, scale,scale,scale) 
+    p2 = lines!(ax, H_exp_x[2:end],H_exp_y[2:end],H_exp_z[2:end], color=:blue,linewidth=2)
+    scale!(p2, scale,scale,scale) 
+
+    xlims!(-10,10)
+    lines!(entropy_ax,s.t_i:s.Δt:s.t_f,entropy,color=:red)
+end 
+    # save("bloch_plot.png",f)
